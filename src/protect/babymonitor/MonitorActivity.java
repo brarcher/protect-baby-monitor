11bf3822b60d060953e88191228a6aca4aea8257
Branden Archer <b.m.archer4@gmail.com>
2015-12-26 15:49:12 -0500

    Add MonitorActivity - for registering on network and sending audio
    This commit adds the shell of the MonitorActivity. This activity
    will register its service on the local network, wait for a connection,
    then send audio data.

--- src/protect/babymonitor/MonitorActivity.java
+++ src/protect/babymonitor/MonitorActivity.java
@@ -17,35 +17,49 @@
 package protect.babymonitor;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class StartActivity extends Activity
+public class MonitorActivity extends Activity
 {
     final String TAG = "BabyMonitor";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        Log.i(TAG, "Baby monitor launched");
-
+        Log.i(TAG, "Baby monitor start");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_monitor);
+    }
 
-        final Button monitorButton = (Button) findViewById(R.id.startMonitorButton);
-        monitorButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Log.i(TAG, "Starting up monitor");
+    @Override
+    protected void onDestroy()
+    {
+        Log.i(TAG, "Baby monitor stop");
 
-                Intent i = new Intent(getApplicationContext(), MonitorActivity.class);
-                startActivity(i);
-            }
-        });
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
