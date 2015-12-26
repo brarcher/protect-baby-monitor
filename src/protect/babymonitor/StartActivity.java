7bd19f2325241ec5b397ef19bd64f364da3a455d
Branden Archer <b.m.archer4@gmail.com>
2015-12-26 15:48:42 -0500

    Add StartActivity - landing page of baby monitor
    The StartActivity is the first activity which will be launched
    in the baby monitor, and in the future will list the two main
    options: become a monitor or a listener.

---
+++ src/protect/babymonitor/StartActivity.java
@@ -0,0 +1,38 @@
+/**
+ * This file is part of the Protect Baby Monitor.
+ *
+ * Protect Baby Monitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Protect Baby Monitor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Protect Baby Monitor. If not, see <http://www.gnu.org/licenses/>.
+ */
+package protect.babymonitor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends Activity
+{
+    final String TAG = "BabyMonitor";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Log.i(TAG, "Baby monitor launched");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
