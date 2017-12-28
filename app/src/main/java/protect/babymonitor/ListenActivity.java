1c64b15c00acbc691499ef4fa85c05728d400bb8
Branden Archer <b.m.archer4@gmail.com>
2017-12-27 19:03:46 -0500

    remove unnecessary Runnable in onCreate()
    onCreate is already running on the UI thread, no need
    to create a separate runnable.

--- app/src/main/java/protect/babymonitor/ListenActivity.java
+++ app/src/main/java/protect/babymonitor/ListenActivity.java
@@ -105,28 +105,21 @@ public class ListenActivity extends Activity
 
         setContentView(R.layout.activity_listen);
 
-        ListenActivity.this.runOnUiThread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                NotificationCompat.Builder mBuilder =
-                        new NotificationCompat.Builder(ListenActivity.this)
-                                .setOngoing(true)
-                                .setSmallIcon(R.drawable.listening_notification)
-                                .setContentTitle(getString(R.string.app_name))
-                                .setContentText(getString(R.string.listening));
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(ListenActivity.this)
+                        .setOngoing(true)
+                        .setSmallIcon(R.drawable.listening_notification)
+                        .setContentTitle(getString(R.string.app_name))
+                        .setContentText(getString(R.string.listening));
 
+        _mNotifyMgr.notify(mNotificationId, mBuilder.build());
 
-                _mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        final TextView connectedText = (TextView) findViewById(R.id.connectedTo);
+        connectedText.setText(_name);
 
-                final TextView connectedText = (TextView) findViewById(R.id.connectedTo);
-                connectedText.setText(_name);
+        final TextView statusText = (TextView) findViewById(R.id.textStatus);
+        statusText.setText(R.string.listening);
 
-                final TextView statusText = (TextView) findViewById(R.id.textStatus);
-                statusText.setText(R.string.listening);
-            }
-        });
 
         _listenThread = new Thread(new Runnable()
         {
