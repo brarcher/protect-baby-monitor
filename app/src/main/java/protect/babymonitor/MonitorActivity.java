e1c44ca66accfb6d8816d379334522868ffbd7a3
Branden Archer <b.m.archer4@gmail.com>
2018-02-26 21:03:03 -0500

    Remove unused settings menu
    The settings menu was present in the Child Device view, but
    did not do anything when selected.

--- app/src/main/java/protect/babymonitor/MonitorActivity.java
+++ app/src/main/java/protect/babymonitor/MonitorActivity.java
@@ -203,27 +203,6 @@ public class MonitorActivity extends Activity
         super.onDestroy();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void registerService(final int port)
     {
         final NsdServiceInfo serviceInfo  = new NsdServiceInfo();
